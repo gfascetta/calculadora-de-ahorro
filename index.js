fc38e9191a11b926dd2e59e2bb0e1ce08f07513e
Gabriela <gfascetta@gmail.com>
2021-10-25 18:42:38 -0300

    empece a manipular el dom

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const calculoAhorroAnualSimple = () => {
     return sumatoriaHasta(365)
 }
 
-calculoAhorroAnualSimple()
-
 const sumatoriaConDuplicadosHasta = (maximo, topeDuplicado) => {
     let acumulador = 0
     for (let i = 1; i <= maximo; i++) {
@@ -24,8 +22,53 @@ const sumatoriaConDuplicadosHasta = (maximo, topeDuplicado) => {
     return acumulador
 }
 
+const sumatoriaMultiplicadaPorHasta = (maximo, multiplicador, topeDuplicado) => {
+    let acumulador = 0
+    for (let i = 1; i <= maximo; i++) {
+        if (i <= topeDuplicado) {
+            acumulador += i * multiplicador
+        } else {
+            acumulador += i
+        }
+    }
+    return acumulador
+}
+
 const calculoAhorroAnualOptimizandoHasta = (tope) => {
     return sumatoriaConDuplicadosHasta(365, tope)
 }
 
-calculoAhorroAnualOptimizandoHasta(150)
+//--------cuanto-section--------------------------------------------------
+const seccionCuanto = document.querySelector('.cuanto-section')
+const radios = document.getElementsByName('tiempo')
+
+
+
+
+
+const ahorroContainer = document.createElement('div')
+ahorroContainer.classList.add('resultado')
+ahorroContainer.innerHTML = `
+<p>Podés ahorrar desde ${} pesos</p>`
+
+
+
+
+//----seccion propuesta--------------------------------------------------------------
+const seccionPropuesta = document.querySelector('.propuesta-section')
+const diasAMultiplicar = document.querySelector('#diasAMultiplicar')
+const resultadoContainer = document.createElement('div')
+resultadoContainer.classList.add('resultado')
+resultadoContainer.innerHTML = `
+<p>Con este plan podés ahorrar ${} pesos</p>`
+
+
+
+const botonMultiplicador = document.querySelector('#botonMultiplicador')
+
+botonMultiplicador.addEventListener('click', calcularAhorroMultiplicado)
+
+function calcularAhorroMultiplicado() {
+
+    seccionPropuesta.appendChild(resultadoContainer)
+}
